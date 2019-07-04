4352f4486a5060b9d80c9664ea228a34e134225d
subarna mukhiyabeen <smukhiyabe@gannett.com>
2019-07-03 21:50:39 -0400

    added campgrounds page, header, footer, and linked bootstrap

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const campgrounds = [
+    {name: 'Salmon Creek', image: 'https://cdn.pixabay.com/photo/2018/10/28/16/58/lake-3779280_640.jpg'},
+    {name: 'Granite Lake', image: 'https://cdn.pixabay.com/photo/2016/08/28/17/05/camping-1626412_640.jpg'},
+    {name: 'Red Rocks', image: 'https://cdn.pixabay.com/photo/2015/05/23/00/25/utah-780108_640.jpg'},    
+]
+
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-    res.send('initial setup')
+    res.render('landing');
+});
+
+app.get('/campgrounds', (req, res) => {
+    res.render('campgrounds', {campgrounds, campgrounds})
 });
 
 app.listen(port, () => {
