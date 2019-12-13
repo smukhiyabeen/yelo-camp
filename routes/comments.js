078c6df1759a6844dc4a0bb5cfd238d6ca6ba2c8
subarna karki <smukhiyabe@gannett.com>
2019-12-13 00:59:24 -0500

    add delete route for comments

--- routes/comments.js
+++ routes/comments.js
@@ -80,5 +80,15 @@ router.put('/:comment_id/', (req, res) => {
   });
 });
 
+// Comments DESTROY route
+router.delete('/:comment_id', (req, res) => {
+  Comment.findByIdAndRemove(req.params.comment_id, (err) => {
+    if (err) {
+      res.redirect('back');
+    } else {
+      res.redirect(`/campgrounds/${req.params.id}`);
+    }
+  });
+});
 
 module.exports = router;
