502d173b1b9d78464ffb29bc079ab7f33ad69181
subarna karki <smukhiyabe@gannett.com>
2019-12-12 19:57:47 -0500

    Add delete route and button

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -86,4 +86,15 @@ router.put('/:id', (req, res) => {
 });
 
 
+// DESTROY ROUTE
+router.delete('/:id', (req, res) => {
+  Campground.findByIdAndRemove(req.params.id, (err) => {
+    if (err) {
+      res.redirect('/campgrounds');
+    } else {
+      res.redirect('/campgrounds');
+    }
+  });
+});
+
 module.exports = router;
